4a68b974ac84efa78b626f61e037e6b38eba10e7
hana_dempsey <hanademps@gmail.com>
2018-06-30 23:14:33 -0700

    create artillery yml file

--- server/index.js
+++ server/index.js
@@ -1,8 +1,10 @@
+require('newrelic');
 const express = require('express');
 const parser = require('body-parser');
 const path = require('path');
 const fs = require('fs');
 const server = require('http').createServer();
+
 
 // const { User, Question } = require('../database/schemas.js');
 const {
@@ -30,6 +32,8 @@ server.on('request', (req, res) => {
 });
 
 app.post('/api/questions', (req, res) => {
+    console.log('posting a question')
+    let date = new Date()
     // db.Question.create({
     //         user_id: req.body.user_id,
     //         restaurant_id: req.body.restaurant_id,
@@ -45,15 +49,40 @@ app.post('/api/questions', (req, res) => {
     //         console.log('Error inserting data into the database', err);
     //         res.status(400).send(err);
     //     })
-    Question.insert({
+    Question.create({
         user_id: req.body.user_id,
-                restaurant_id: req.body.restaurant_id,
-                text: req.body.text,
-                parent_id: req.body.parent_id || null,
-                helpful: req.body.helpful || null
+        restaurant_id: req.body.restaurant_id,
+        text: req.body.text,
+        parent_id: 5000001,
+        helpful: 0
     }).then(data => {
         console.log('data was posted to questions')
+        res.send("data posted")
     })
+})
+
+app.delete('/api/questions', (req, res) => {
+    console.log('delete:', req.body)
+    Question.deleteOne({
+        parent_id: req.body.parent_id
+    }).then(data => {
+        res.send('data was deleted')
+    })
+})
+
+app.put('/api/questions', (req, res) => {
+    console.log('something is being put', req.body)
+    Question.updateOne({
+        parent_id: req.body.parent_id
+    }, {
+        $set: {
+            parent_id: req.body.update
+        }
+    }).then(data => {
+        console.log('data in put',data)
+        res.send('put was placed')
+    })
+    
 })
 
 // app.post('/api/users', (req, res) => {
@@ -69,8 +98,7 @@ app.post('/api/questions', (req, res) => {
 //         })
 // })
 
-app.get('/api/questions/:id', (req, res) => {
-    console.log('HERE>>>>>>>', req.params)
+app.get('/api/questions', (req, res) => {
     // db.Question.find({
     //         restaurant_id: req.params
     //     })
@@ -81,13 +109,11 @@ app.get('/api/questions/:id', (req, res) => {
     //         res.send(err);
     //     })
     Question.find({
-        // restaurant_id: req.params.id
-        type: 1
-    }).limit(5)
-    .then(data => {
-        console.log('Data received', data)
-        res.status(200).send(data);
-    })
+        $and:[{ restaurant_id: 2 }, {type: 1}]
+        }).limit(5).then(data => {
+            console.log('Data received')
+            res.status(200).send(data);
+        })
 })
 
 // app.get(`/api/getPhoto/:id`, (req, res) => {
@@ -105,25 +131,25 @@ app.get('/api/questions/:id', (req, res) => {
 // })
 
 app.get("/api/getAnswers/:id", (req, res) => {
-        // Question.find({
-        //         where: {
-        //             parent_id: req.params.id
-        //         }
-        //     })
-        //     .then(data => {
-        //         res.status(200).json(data);
-        //     })
-        //     .catch(err => {
-        //         res.status(400).json(err);
-        //     })
+    // Question.find({
+    //         where: {
+    //             parent_id: req.params.id
+    //         }
+    //     })
+    //     .then(data => {
+    //         res.status(200).json(data);
+    //     })
+    //     .catch(err => {
+    //         res.status(400).json(err);
+    //     })
     console.log('req.params.id', req.params.id)
     Question.find({
-        parent_id: req.params.id
-    })
-    .then(data => {
-        console.log('Answers recieved')
-        res.status(200).send(data);
-    })
+            parent_id: req.params.id
+        })
+        .then(data => {
+            console.log('Answers recieved')
+            res.status(200).send(data);
+        })
 })
 
 app.listen(PORT, () => {

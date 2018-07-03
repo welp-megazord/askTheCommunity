ad76a20c5df41955611d0eca04850acfab487392
hana_dempsey <hanademps@gmail.com>
2018-07-02 20:51:21 -0700

    refractor routes and clean up code

--- server/index.js
+++ server/index.js
@@ -5,8 +5,6 @@ const path = require('path');
 const fs = require('fs');
 const server = require('http').createServer();
 
-
-// const { User, Question } = require('../database/schemas.js');
 const {
     User,
     Question
@@ -34,21 +32,6 @@ server.on('request', (req, res) => {
 app.post('/api/questions', (req, res) => {
     console.log('posting a question')
     let date = new Date()
-    // db.Question.create({
-    //         user_id: req.body.user_id,
-    //         restaurant_id: req.body.restaurant_id,
-    //         text: req.body.text,
-    //         parent_id: req.body.parent_id || null,
-    //         helpful: req.body.helpful || null
-    //     })
-    //     .then(data => {
-    //         console.log('Succesfully inserted data into the database...', data);
-    //         res.status(201).send(data);
-    //     })
-    //     .catch(err => {
-    //         console.log('Error inserting data into the database', err);
-    //         res.status(400).send(err);
-    //     })
     Question.create({
         user_id: req.body.user_id,
         restaurant_id: req.body.restaurant_id,
@@ -98,18 +81,10 @@ app.put('/api/questions', (req, res) => {
 //         })
 // })
 
-app.get('/api/questions', (req, res) => {
-    // db.Question.find({
-    //         restaurant_id: req.params
-    //     })
-    //     .then(data => {
-    //         res.status(200).json(data);
-    //     })
-    //     .catch(err => {
-    //         res.send(err);
-    //     })
+app.get('/api/questions/:id', (req, res) => {
+    console.log('req.params:', req.params.id)
     Question.find({
-        $and:[{ restaurant_id: 2 }, {type: 1}]
+        $and:[{ restaurant_id: req.params.id}, {type: 1}]
         }).limit(5).then(data => {
             console.log('Data received')
             res.status(200).send(data);
@@ -131,17 +106,6 @@ app.get('/api/questions', (req, res) => {
 // })
 
 app.get("/api/getAnswers/:id", (req, res) => {
-    // Question.find({
-    //         where: {
-    //             parent_id: req.params.id
-    //         }
-    //     })
-    //     .then(data => {
-    //         res.status(200).json(data);
-    //     })
-    //     .catch(err => {
-    //         res.status(400).json(err);
-    //     })
     console.log('req.params.id', req.params.id)
     Question.find({
             parent_id: req.params.id
